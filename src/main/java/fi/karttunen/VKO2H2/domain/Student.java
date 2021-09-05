4f27c7fd501c92ea1ec30af2fccb697ea153c86d
Toni Karttunen <toni.karttunen@gmail.com>
2021-09-05 14:55:29 +0300

    Valmis ja toimiva versio

--- src/main/java/fi/karttunen/VKO2H2/domain/Student.java
+++ src/main/java/fi/karttunen/VKO2H2/domain/Student.java
@@ -1,5 +1,38 @@
 package fi.karttunen.VKO2H2.domain;
 
 public class Student {
+	private String firstName;
+	private String lastName;
+	
+	public Student(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
 }
--- src/main/java/fi/karttunen/VKO2H2/web/StudentController.java
+++ src/main/java/fi/karttunen/VKO2H2/web/StudentController.java
@@ -1,5 +1,25 @@
 package fi.karttunen.VKO2H2.web;
 
-public class StudentController {
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.karttunen.VKO2H2.domain.Student;
+
+@Controller
+public class StudentController {
+	
+	ArrayList<Student> students = new ArrayList<>();
+		
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String studentGreeting(Model model) {
+		students.add(new Student("Kate", "Cole"));
+		students.add(new Student("Dan", "Brown"));
+		students.add(new Student("Mike", "Mars"));
+		model.addAttribute("students", students);
+		return "hello";
+	}
 }
